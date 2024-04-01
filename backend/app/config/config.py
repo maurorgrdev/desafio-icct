1b40d4a92b1ae379c44bcac94d10f060b6073db4
Mauro Roger <mauroroger2020@gmail.com>
2024-04-01 05:38:37 -0400

    fix: carregar JWT_SECRET_KEY atraves do .env

--- backend/app/config/config.py
+++ backend/app/config/config.py
@@ -8,7 +8,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY')
-    JWT_SECRET_KEY = '2pYWDgRjgVAr22fKjv-VWNovuV0_0T9p5e4F2s1Cdk0'
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
     JWT_BLACKLIST_ENABLED = True
     JWT_BLACKLIST_TOKEN_CHECKS = ['access', 'refresh']
 
